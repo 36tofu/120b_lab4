8c8ac053eb50e29855d00376e7a79a14c63725a0
36tofu <christopher99chen@gmail.com>
2021-01-25 21:40:04 -0800

    completed part 3

---
+++ turnin/cchen233_lab4_part3.c
@@ -0,0 +1,92 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum states { start, lock, pound, waitY, unlock } state;
+
+unsigned char tmpA;
+
+void tick()
+{
+	tmpA = PINA & 0x87;
+	switch(state){
+		case start:
+			state = lock;
+			break;
+		case lock:
+			if(tmpA == 4)
+				state = pound;
+			else 
+				state = lock;
+			break;
+		case pound:
+			if(tmpA == 4)
+				state = pound;
+			else if(tmpA == 0)
+				state = waitY;
+			break;
+		case waitY:
+			if(tmpA == 0)
+				state = waitY;
+			else if(tmpA == 2)
+				state = unlock;
+			else
+				state = lock;
+			break;
+		case unlock:
+			if(tmpA == 0x80)
+				state = lock;
+			else 
+				state = unlock;
+			break;
+		default:
+			state = start;
+			break;
+		
+	}
+	//state actions
+	switch(state){
+		case start:
+			PORTB = 0;
+			break;
+		case lock:
+			PORTB = 0;
+			break;
+		case pound:
+			PORTB = 0;
+			break;
+		case waitY:
+			PORTB = 0;
+			break;
+		case unlock:
+			PORTB = 1;
+			break;
+		default:
+			break;
+		
+	}
+	PORTC = state;
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as inputs
+    /* Insert your solution below */
+	state = start;   
+	while (1) {
+		tick();
+    	}
+   	 return 1;
+}
